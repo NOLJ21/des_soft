ba0a19035e0343dca9a2cd56dc66fe1aeee16acc
NOLJ21 <nikolai.lopez@ucb.edu.bo>
2023-03-29 17:11:04 -0400

    login agregado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EditarPerfilComponent } from './editar-perfil/editar-perfil.component';
 
-const routes: Routes = [];
+const routes: Routes = [ // route es un array de objetos
+  { path: 'editar-perfil', component: EditarPerfilComponent }
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
